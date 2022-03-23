e292e59e4a933bd65b5c26fd361033d84f073550
squidyp <squidy.p@gmail.com>
2022-03-22 17:21:42 -0700

    updating

--- goWebServer/main.go
+++ goWebServer/main.go
@@ -8,10 +8,10 @@ import (
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "Parseform() err: %v", err)
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful ")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s \n", name)
