6b737af22a4d204dac47ca43b4a3b9d0a442c102
Charles Panter <squidy.p@gmail.com>
2023-09-05 15:20:13 -0700

    built a small example crud app

--- goWebServer/main.go
+++ goWebServer/main.go
@@ -11,7 +11,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful ")
+	fmt.Fprintf(w, "POST request successful \n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s \n", name)
